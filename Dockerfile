8a101e199a6c5d2659934ce5b77641b767c5080d
Harsh Raj <harshbasketball06@gmail.com>
2024-03-31 17:35:25 +0530

    initial commit

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17-alpine
-EXPOSE 9090
-ADD target/spring-boot-docker.jar spring-boot-docker.jar
-ENTRYPOINT ["java", "-jar", "/spring-boot-docker.jar"]
--- src/main/java/com/nagarro/training/miniassignmenttwo/service/GenderApiService.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.training.miniassignmenttwo.service;
-
-import reactor.core.publisher.Mono;
-
-public interface GenderApiService {
-
-	String getGender(String name);
-
-}
